84c58a2e8a351e0262522f07ca96a1943b5c8785
Charlie Thomas <charlie.thomas@attwoodthomas.net>
2019-12-28 00:44:49 +0000

    Finish unquote

--- evaluator/quote_unquote_test.go
+++ evaluator/quote_unquote_test.go
@@ -63,6 +63,33 @@ func TestQuoteUnquote(t *testing.T) {
 			`quote(unquote(4 + 4) + 8)`,
 			`(8 + 8)`,
 		},
+		{
+			`let foobar = 8;
+			quote(foobar)`,
+			`foobar`,
+		},
+		{
+			`let foobar = 8;
+			quote(unquote(foobar))`,
+			`8`,
+		},
+		{
+			`quote(unquote(true))`,
+			`true`,
+		},
+		{
+			`quote(unquote(true == false))`,
+			`false`,
+		},
+		{
+			`quote(unquote(quote(4 + 4)))`,
+			`(4 + 4)`,
+		},
+		{
+			`let quotedInfixExpresion = quote(4 + 4);
+					quote(unquote(4 + 4) + unquote(quotedInfixExpresion))`,
+			`(8 + (4 + 4))`,
+		},
 	}
 
 	for _, tt := range tests {
